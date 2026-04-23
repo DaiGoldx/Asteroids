12947f1633843d52c83e56c5de11e7b020dd129e
DaiGoldx <jakubdaleczko@gmail.com>
2026-04-23 22:07:53 +0200

    CH3: Player L2: Draw Player

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -1,12 +1,15 @@
 from constants import *
 from logger import log_state
 import pygame
+from player import Player
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     version = pygame.version.ver
     fps_clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     print(f"Starting Asteroids with pygame version: {version}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -16,6 +19,7 @@ def main():
                 if event.type == pygame.QUIT:
                     return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = fps_clock.tick(60)/1000
         #print(dt)
