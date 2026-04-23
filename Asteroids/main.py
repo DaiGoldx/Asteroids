0a200a4812dc42a009f739d6e4303ce929a7c362
DaiGoldx <jakubdaleczko@gmail.com>
2026-04-23 23:34:52 +0200

    CH4: Asteroids L1: Asteroids, added game object asteroid and its spawn field

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -2,6 +2,8 @@ from constants import *
 from logger import log_state
 import pygame
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -9,18 +11,27 @@ def main():
     version = pygame.version.ver
     fps_clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroidField = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     print(f"Starting Asteroids with pygame version: {version}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     while True:
         log_state()
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     return
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
+        updatable.update(dt)
         pygame.display.flip()
         dt = fps_clock.tick(60)/1000
         #print(dt)
