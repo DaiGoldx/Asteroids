93f9d3cb4e86a8f4ee20a0b622fa3b45c42cdcac
DaiGoldx <jakubdaleczko@gmail.com>
2026-04-23 22:56:45 +0200

    CH3:Player L3:Moving around, added rotation to play object

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -20,6 +20,7 @@ def main():
                     return
         screen.fill("black")
         player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
         dt = fps_clock.tick(60)/1000
         #print(dt)
